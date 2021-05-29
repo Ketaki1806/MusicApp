1fe608816c76f29799d530e62971e52c3f7a9802
aditinikam <aditirnikam21@gmail.com>
2021-05-29 12:39:25 +0530

    angular updation

--- music-app/src/app/app-routing.module.ts
+++ music-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'category', component: CategoryComponent },
+  { path: 'home', component:HomeComponent},
+  { path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- music-app/src/app/app.module.ts
+++ music-app/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
 import { HomeComponent } from './home/home.component';
 import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent,
     HomeComponent,
     CategoryComponent
   ],
