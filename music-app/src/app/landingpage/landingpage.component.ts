fb143d773666d41385732989fa741e5c9a8b06ba
Ketaki Hadnurkar <ketaki.hadnurkar@gmail.com>
2021-06-30 16:07:57 +0530

    menubar added

--- music-app/src/app/landingpage/landingpage.component.ts
+++ music-app/src/app/landingpage/landingpage.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingpageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() {
+    
+   }
+  
   ngOnInit(): void {
+    const btn = document.getElementById("bar_btn");
+    if(btn)
+      btn.addEventListener("click", toggle);
+    function toggle(){
+      const a = document.getElementById("list");
+      if(a)
+        a.classList.toggle("active");
+      // console.log("*");
+    }
   }
+  
 
 }
